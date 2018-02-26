6c0b941a051a691c159d4b644d886168bd913e47
sergeybolzan <sergey.bolzan@gmail.com>
2018-02-26 17:34:08 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ 	let arr = [];
+	let result = false;
+	for(let i = 0; i < str.length; i++) {
+		let prev = arr[arr.length - 1];
+		let curr = str[i];
+
+		for(let i = 0; i < bracketsConfig.length; i++) {
+			if (prev == bracketsConfig[i][0] && curr == bracketsConfig[i][1]) {
+				arr.pop();
+				result = true;
+				break;
+			}
+		}
+
+		if (result == false) arr.push(curr);
+		result = false;
+	}
+	return arr.length == 0;
 }
